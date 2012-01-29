e66934d10d19d1f3ccf67769bac0cf4981bfd718
Crosant <florian@reichmuth.de>
2012-01-29 19:44:24 +0100

    new file:   pom.xml 	new file:   src/main/java/me/crosant/activate_bukkit_plugin/activate_bukkit_plugin.java

---
+++ src/main/java/me/crosant/activate_bukkit_plugin/activate_bukkit_plugin.java
@@ -0,0 +1,13 @@
+package me.crosant.activate_bukkit_plugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class activate_bukkit_plugin 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
